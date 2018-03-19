c6c55b9512daab086ab1083eca4f3432aab48404
NorihisaOmori <nonchi842@gmail.com>
2018-03-20 02:07:51 +0900

    練習用

--- Sample2/ViewController.swift
+++ Sample2/ViewController.swift
@@ -9,10 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        label.text = "Hello World"
+        
     }
 
     override func didReceiveMemoryWarning() {
